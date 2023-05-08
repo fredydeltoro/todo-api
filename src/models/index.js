f6b1b4c06e8f18ca6fbd8e2036a4bfcf4cff4ddf
Freddy Monroy <fredydeltoro@gmail.com>
2023-05-08 14:06:35 -0600

    Correct use of models

--- src/models/index.js
+++ src/models/index.js
@@ -1,21 +1,16 @@
 const { Sequelize } = require('sequelize');
-const User = require('./User');
-const TodoList = require('./TodoList');
-const TodoItem = require('./TodoItem');
+const user = require('./User');
+const todoList = require('./TodoList');
+const todoItem = require('./TodoItem');
 
-const sequelize = new Sequelize('todo_db', 'username', '', {
+const sequelize = new Sequelize('todo_db', '', '', {
   host: 'localhost',
-  dialect: 'postgress',
+  dialect: 'postgres',
 });
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+const User = user(sequelize);
+const TodoList = todoList(sequelize);
+const TodoItem = todoItem(sequelize);
 
 // Relations
 
@@ -23,5 +18,14 @@ User.hasMany(TodoList);
 TodoList.belongsTo(User);
 TodoList.hasMany(TodoItem);
 TodoItem.belongsTo(TodoList);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log('====== Connection has been established successfully. ======');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
